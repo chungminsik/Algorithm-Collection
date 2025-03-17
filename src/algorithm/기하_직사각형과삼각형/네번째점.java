0774284f613143d713278830a47909dbfa77b129
정민식 <minsik327@gmail.com>
2025-03-17 10:17:08 +0900

    알고리즘 문제 - 직사각형에서 탈출, 네번째 점(마크다운 작성 안함)

---
+++ src/algorithm/기하_직사각형과삼각형/네번째점.java
@@ -0,0 +1,42 @@
+package algorithm.기하_직사각형과삼각형;
+
+import java.util.*;
+
+public class 네번째점 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] x = new int[3];
+        int[] y = new int[3];
+
+        for (int i = 0; i < 3; i++){
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int result_x = findUniqueNumber(x);
+        int result_y = findUniqueNumber(y);
+
+        System.out.println(result_x + " " + result_y);
+
+    }
+
+    public static int findUniqueNumber(int[] arr) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // 숫자의 빈도수 계산
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // 빈도수가 1인 숫자 찾기
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return -1; // 예외 처리 (모든 숫자가 짝이 맞을 경우)
+    }
+}
