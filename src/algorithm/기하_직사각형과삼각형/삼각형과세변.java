dda74063734d4842d76b35831e3a9ff2400d350a
정민식 <minsik327@gmail.com>
2025-03-19 10:05:11 +0900

    알고리즘 문제 - 삼각형과 세변, 삼각형 외우기(마크다운 작성 안함)

---
+++ src/algorithm/기하_직사각형과삼각형/삼각형과세변.java
@@ -0,0 +1,46 @@
+package algorithm.기하_직사각형과삼각형;
+
+import java.util.*;
+
+public class 삼각형과세변 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop){
+            List<Integer> inputList = new ArrayList<>();
+
+            int count = 0;
+            for (int i = 0; i < 3; i++){
+                int input = sc.nextInt();
+                count += input;
+                inputList.add(input);
+            }
+
+            if (count == 0){
+                break;
+            }
+
+            inputList.sort(Collections.reverseOrder());
+
+            //삼각형인지 아닌지
+            if (inputList.get(0) >= inputList.get(1)+inputList.get(2)){
+                System.out.println("Invalid");
+
+            } else if (inputList.get(0).equals(inputList.get(1)) && inputList.get(1).equals(inputList.get(2))) {
+                System.out.println("Equilateral");
+
+            } else if(inputList.get(0).equals(inputList.get(1)) || inputList.get(1).equals(inputList.get(2)) || inputList.get(0).equals(inputList.get(2))){
+                System.out.println("Isosceles");
+
+            } else {
+                System.out.println("Scalene");
+
+            }
+        }
+        sc.close();
+
+    }
+}
+
---
+++ src/algorithm/기하_직사각형과삼각형/삼각형외우기.java
@@ -0,0 +1,20 @@
+package algorithm.기하_직사각형과삼각형;
+
+import java.util.Scanner;
+
+public class 삼각형외우기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b= sc.nextInt(), c = sc.nextInt();
+
+        if ((a + b + c) != 180){
+            System.out.println("error");
+        } else if (a == 60 && b == 60 && c == 60){
+            System.out.println("Equilateral");
+        } else if(a == b || b == c || a == c){
+            System.out.println("Isosceles");
+        } else{
+            System.out.println("Scalene");
+        }
+    }
+}
