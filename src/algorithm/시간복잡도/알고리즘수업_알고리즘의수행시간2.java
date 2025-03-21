fc7016acce24b29c8466c6c00a979e8e845194e7
정민식 <minsik327@gmail.com>
2025-03-21 10:11:03 +0900

    알고리즘 문제 - 알고리즘수업_알고리즘의수행시간1, 알고리즘수업_알고리즘의수행시간2(마크다운 작성 안함)

---
+++ src/algorithm/시간복잡도/알고리즘수업_알고리즘의수행시간2.java
@@ -0,0 +1,13 @@
+package algorithm.시간복잡도;
+
+import java.util.Scanner;
+
+public class 알고리즘수업_알고리즘의수행시간2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int result = sc.nextInt();
+        System.out.println(result);
+        System.out.println(1);
+    }
+}
