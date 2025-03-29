cfff36c17737098f0b2757aec743b7cde0bc80df
minsik <minsik327@gmail.com>
2025-03-29 14:41:08 +0900

    面接準備2

--- src/algorithm/bitA/Q1.java
+++ src/algorithm/bitA/Q1.java
@@ -3,7 +3,7 @@ package algorithm.bitA;
 /**
  * 1. 순열을 만든다 2. 계산을 한다 3. 계산한 꼭대기를 모두 더한다
  *
- * 계산은 쉬워보여서 하나의 순열을 계산하는 로직을 만듦
+ * 계산은 쉬워보여서 하나의 순열을 꼭대기 값을 계산하는 로직을 만듦
  * 많은 순열을 계산해야 하기 때문에 메소드화
  *
  * 순열을 만드는 방법을 모색(중복되지 않는 숫자, 중복되지 않은 조합의 9자리 숫자배열을 전부 만들기)
@@ -16,41 +16,6 @@ package algorithm.bitA;
 
 public class Q1 {
 
-    public static void main(String[] args){
-
-        int totalResult = 0;
-        //int[] array = new int[9];
-        int[] array = {1,2,3,4,9,8,7,6,5};
-
-        //순열을 만든다
-
-
-        //계산한다 + 더한다
-        boolean loop = true;
-        while(loop){
-            if (array.length == 1){
-                totalResult = array[0];
-                loop = false;
-                break;
-            }
-
-            int[] newArray = new int[array.length-1];
-
-            for (int i = 0; i < array.length-1; i++){
-                newArray[i] = (array[i] + array[i+1]) % 10;
-                array = newArray;
-            }
-        }
-
-        System.out.println(totalResult);
-
-
-    }
-
-
-}
-
-class Hello{
     private static long totalSum = 0; // 모든 순열에 대한 꼭대기 값의 누적 합
 
     public static void main(String[] args) {
@@ -58,15 +23,17 @@ class Hello{
         int[] permArray = new int[9];
         boolean[] usedNum = new boolean[9];
 
-        generatePerm(permArray, usedNum, 0);
+        executeCompute(permArray, usedNum, 0);
+
         System.out.println(totalSum);
     }
 
-
     /*
     1, 2, 3, 4, 5, 6, 7, 8, 9 까지 만들고 뒤에서부터 바꿔가면서 경우의 수를 전부 확인
+    123456789 라는 기준을 정하고 거기서 부터 순차적으로 바꾸면서 계산 진행
      */
-    private static void generatePerm(int[] perm, boolean[] usedNum, int depth){
+    private static void executeCompute(int[] perm, boolean[] usedNum, int depth){
+
         if (depth == 9){
             totalSum += getTopNumber(perm);
             return;
@@ -76,7 +43,7 @@ class Hello{
             if (!usedNum[i]){
                 perm[depth] = i;
                 usedNum[i] = true;
-                generatePerm(perm, usedNum, depth+1);
+                executeCompute(perm, usedNum, depth+1);
 
                 //만약 depth가 9가 되면 숫자를 더하고 여기로 리턴됨
                 usedNum[i] = false;
@@ -103,5 +70,3 @@ class Hello{
         return getTopNumber(newArray);
     }
 }
-
-
