075481d0c4e73a15d4e19467e7bd6f2d45ed60c6
정민식 <minsik327@gmail.com>
2025-04-28 17:57:39 +0900

    dfs 연습3

---
+++ src/algorithm/재귀/피보나치수5.java
@@ -0,0 +1,24 @@
+package algorithm.재귀;
+
+import java.util.Scanner;
+
+public class 피보나치수5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fibo(n));
+
+    }
+
+    public static int fibo(int n){
+        if (n == 0 || n == 1){
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
+
+}
