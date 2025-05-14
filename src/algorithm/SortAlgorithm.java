93a52f44064db7002b518c80d85039d9b1aac86b
minsik <minsik327@gmail.com>
2025-05-14 23:56:03 +0900

    マージソート練習

--- src/algorithm/SortAlgorithm.java
+++ src/algorithm/SortAlgorithm.java
@@ -5,8 +5,10 @@ public class SortAlgorithm {
 
         int[] unSortedArray = {3, 5, 1, 4, 2};
 
-        printSortResult(selectionSort(unSortedArray));
-        printSortResult(insertSort(unSortedArray));
+        //printSortResult(selectionSort(unSortedArray));
+        //printSortResult(insertSort(unSortedArray));
+        //printSortResult(mergeSort(unSortedArray, 0, unSortedArray.length-1));
+        printSortResult(helloMergeSort(unSortedArray, 0, unSortedArray.length-1));
     }
 
     public static void printSortResult(int[] array){
@@ -69,6 +71,148 @@ public class SortAlgorithm {
 
         return sortedArray;
     }
+
+    //順番に分割の命令（分割範囲を指定してメソッドに入れる）
+    //そのあとマージを行う
+    public static int[] mergeSort(int[] array, int left, int right){
+
+        //要素が一つになってない場合
+        if (left < right){
+            int mid = (left + right) / 2;
+
+            //arrayを分割するスタックを作る
+            mergeSort(array, left, mid); //array1
+            mergeSort(array, mid+1, right);//array2
+
+            //mergeする
+            int[] merged = merge(array, left, mid, right);
+
+            for (int i = 0; i< merged.length; i++){
+                array[left+i] = merged[i];
+            }
+        }
+
+        return array;
+    }
+
+    //分割、定列
+    public static int[] merge(int[] array, int left, int mid, int right){
+        int[] temp = new int[right - left + 1];
+
+        int i = left; //array1の先頭
+        int j = mid + 1;//array2の先頭
+        int k = 0;//tempの先頭
+
+        //array1とarray2どちらかが比較が終わるまで終わるまで比較して統合
+        while(i <= mid && j <= right){
+            if (array[i] <= array[j]){
+                temp[k++] = array[i++];
+            } else{
+                temp[k++] = array[j++];
+            }
+        }
+
+        //残りの要素を入れる
+        while (i <= mid){
+            temp[k++] = array[i++];
+        }
+        while (j <= right){
+            temp[k++] = array[j++];
+        }
+
+        return temp;
+    }
+
+
+
+
+
+
+    public static int[] helloMergeSort(int[] array, int start, int end){
+
+        if (start < end){
+            int middle = (start + end) / 2;
+
+            helloMergeSort(array, start, middle);
+            helloMergeSort(array, middle + 1, end);
+
+            int[] merged = helloMerge(array, start, middle, end);
+
+            for (int i = 0; i < merged.length; i++){
+                array[start+i] = merged[i];
+            }
+        }
+
+        return array;
+    }
+
+    public static int[] helloMerge(int[] array, int start, int middle, int end){
+        int[] temp = new int[end - start + 1];
+
+        int firstArrayHead = start;
+        int secondArrayHead = middle + 1;
+        int tempHead = 0;
+
+        while(firstArrayHead <= middle && secondArrayHead <= end){
+            if (array[firstArrayHead] <= array[secondArrayHead]){
+                temp[tempHead++] = array[firstArrayHead++];
+            } else {
+                temp[tempHead++] = array[secondArrayHead++];
+            }
+        }
+
+        while (firstArrayHead <= middle){
+            temp[tempHead++] = array[firstArrayHead++];
+        }
+        while (secondArrayHead <= end){
+            temp[tempHead++] = array[secondArrayHead++];
+        }
+
+        return temp;
+    }
+
+
+
+
+
+
+
+
+
+    static int[] buff;
+    static void mergeSort(int[] a, int n) {
+        buff = new int[n];			// 作業用配列を生成
+
+        __mergeSort(a, 0, n - 1);		// 配列全体をマージソート
+
+        buff = null;				// 作業用配列を解放
+    }
+    //--- a[left]～a[right]を再帰的にマージソート ---//
+    static void __mergeSort(int[] a, int left, int right) {
+        if (left < right) {
+            int i;
+            int center = (left + right) / 2;
+            int p = 0;
+            int j = 0;
+            int k = left;
+
+            __mergeSort(a, left, center);        // 前半部をマージソート
+            __mergeSort(a, center + 1, right);    // 後半部をマージソート
+
+            for (i = left; i <= center; i++) {    //前半部分をbuffにコピー　①
+                buff[p++] = a[i];
+            }
+            while (i <= right && j < p) {        //後半部分とbuffをマージ ②
+                a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
+            }
+            while (j < p) {                      //buffに残った要素をaにコピー ③
+                a[k++] = buff[j++];
+            }
+        }
+    }
 }
+
+
+
 
 
